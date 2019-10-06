3e39fa441122d01ab671d43e0b2de5bbea8aff24
Sangin <sangin.kang@navercorp.com>
2019-10-06 16:49:33 +0900

    #5.1 Footer and Auth UI part Two

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React from "react";
+import { gql } from "apollo-boost";
 import GlobalStyles from "../Styles/GlobalStyles";
-import {ThemeProvider} from "styled-components";
+import styled, { ThemeProvider } from "styled-components";
 import Theme from "../Styles/Theme";
 import Router from "./Router";
+import { useQuery } from "react-apollo-hooks";
+import Footer from "./Footer";
 
-export default () => (
-  <ThemeProvider theme={Theme}>
-    <>
-      <GlobalStyles/>
-      <Router isLoggedIn={true} />
-    </>
+const QUERY = gql`
+  {
+    isLoggedIn @client
+  }
+`;
 
-  </ThemeProvider>
-)
+const Wrapper = styled.div`
+  margin: 0 auto;
+  max-width: 935px;
+  width: 100%
+`;
+
+export default () => {
+  const { data: { isLoggedIn } } = useQuery(QUERY);
+
+  return (
+    <ThemeProvider theme={Theme}>
+      <Wrapper>
+        <GlobalStyles />
+        <Router isLoggedIn={isLoggedIn} />
+        <Footer/>
+      </Wrapper>
+    </ThemeProvider>
+  );
+};
