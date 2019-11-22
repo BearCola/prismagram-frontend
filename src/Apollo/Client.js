ff5040a1cf3d722f134d6ca636c7b8127a97c873
Sangin <sangin.kang@navercorp.com>
2019-11-23 06:47:39 +0900

    Change endpoint

--- src/Apollo/Client.js
+++ src/Apollo/Client.js
@@ -2,7 +2,10 @@ import ApolloClient from "apollo-boost";
 import { resolvers, defaults } from "./LocalState";
 
 export default new ApolloClient({
-  uri: "http://localhost:4000",
+  uri:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:4000"
+      : "https://prismagram-backend7.herokuapp.com/",
   clientState: {
     defaults,
     resolvers
